c1589f532bcd5986dd9cebf36b64f61eebdab369
Kristopher Kram <creambulle@gmail.com>
2021-12-10 22:31:29 +0100

    Fler Frågor

--- src/shared/data/quizQuestions.js
+++ src/shared/data/quizQuestions.js
@@ -35,14 +35,14 @@ export const quizQuestions = [
     correctAnwser: "Enhörning",
   },
   {
-    question: "vilket är världens största land?",
-    questionAlternatives: ["Kina", "Ryssland", "Indien", "Australien"],
-    correctAnwser: "Ryssland",
+    question: "Hur många spelare finns det i ett baseboll lag?",
+    questionAlternatives: ["8", "9", "10", "11"],
+    correctAnwser: "9",
   },
   {
-    question: "Vad heter Kanadas huvudstad?",
-    questionAlternatives: ["Ottawa", "Perth", "Edmonton", "Sacramento"],
-    correctAnwser: "Ottawa",
+    question: "Vad kallas för Hallux på människokroppen?",
+    questionAlternatives: ["Hjärtat", "Pekfingret", "Stortån", "Tungan"],
+    correctAnwser: "Stortån",
   },
   {
     question: "När öppnade London sin tunnelbana?",
@@ -53,5 +53,77 @@ export const quizQuestions = [
     question: "Hur många tangenter har ett klassiskt piano?",
     questionAlternatives: ["101", "61", "76", "88"],
     correctAnwser: "88",
+  },
+  {
+    question:
+      "Ett par som firar sin kristallbröllopsdag har varit gifta i hur många år?",
+    questionAlternatives: ["5", "15", "30", "50"],
+    correctAnwser: "15",
+  },
+  {
+    question: "Vad heter Batmans butler?",
+    questionAlternatives: ["Max", "Edwin", "Alfred", "Jarvis"],
+    correctAnwser: "Alfred",
+  },
+  {
+    question: "Vilken karaktär finns INTE med i brädspelet Cluedo?",
+    questionAlternatives: [
+      "Fröken Röd",
+      "Överste Senap",
+      "Doktor Orkidé",
+      "Direktör Plommon",
+    ],
+    correctAnwser: "Direktör Plommon",
+  },
+  {
+    question:
+      "Vilken färg på bältet följer det orange bältet i kampsporten Judo?",
+    questionAlternatives: ["Svart", "Gul", "Grön", "Rosa"],
+    correctAnwser: "Grön",
+  },
+  {
+    question: "Vad heter Facebooks grundare?",
+    questionAlternatives: [
+      "Jawed Karim",
+      "Jeff Bezos",
+      "Mark Zuckerberg",
+      "Steve Chen",
+    ],
+    correctAnwser: "Mark Zuckerberg",
+  },
+  {
+    question: "Hur många länder gränsar till Norge?",
+    questionAlternatives: ["1", "2", "3", "4"],
+    correctAnwser: "3",
+  },
+  {
+    question: "Vad heter världens första spelkonsol för hemmet?",
+    questionAlternatives: [
+      "Nintendo NES",
+      "Atari 2600",
+      "Atari Home Pong",
+      "Magnavox Odyssey",
+    ],
+    correctAnwser: "Magnavox Odyssey",
+  },
+  {
+    question: "Vilket djur ser man på bilmärket Porsche?",
+    questionAlternatives: ["Tjur", "Häst", "Skorpion", "Hund"],
+    correctAnwser: "Häst",
+  },
+  {
+    question: "Vilket land konsumerar mest choklad per capita?",
+    questionAlternatives: ["Frankrike", "USA", "Norge", "Schweiz"],
+    correctAnwser: "Schweiz",
+  },
+  {
+    question: "Vilket land uppfann glassen?",
+    questionAlternatives: ["Kina", "Italien", "Tyskland", "Mexiko"],
+    correctAnwser: "Kina",
+  },
+  {
+    question: "Hur många färger finns det i regnbågen?",
+    questionAlternatives: ["6", "7", "8", "9"],
+    correctAnwser: "7",
   },
 ];
